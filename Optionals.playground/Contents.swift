efe6075e25051466e11ef2d2554813358b59cb7d
mcgraths7 <mcgrath.steven7@gmail.com>
2018-08-15 19:55:21 -0400

    lesson complete

--- Optionals.playground/Contents.swift
+++ Optionals.playground/Contents.swift
@@ -2,42 +2,32 @@ func brokenFirstItem(items: [String]) -> String {
     return items[0]
 }
 
-var fruits = ["apple", "banana", "grape"]
-brokenFirstItem(items: fruits)  // Works!
-fruits = []
-// Uncomment the next line to see the problem.
-// You will get an "index out of range" error.
-//brokenFirstItem(fruits)  // Breaks :-(
+var items = ["apple", "orange", "banana"]
 
+print(brokenFirstItem(items: items))
 
+items = []
+
+//print(brokenFirstItem(items: items))
 
 func firstItem(items: [String]) -> String? {
-    if items.isEmpty {
+    if items.isEmpty{
         return nil
+    } else {
+        return items[0]
     }
-    return items[0]
 }
 
-fruits = ["apple", "banana", "grape"]
-firstItem(items: fruits)
+print(firstItem(items: items))
 
-if let fruit = firstItem(items: fruits) {
+
+if let fruit = firstItem(items: items) {
     print("The first fruit is \(fruit)")
 } else {
-    print("There are no fruits!")
+    print("there is no fruit")
 }
 
-fruits = []
-firstItem(items: fruits)
-
-if let fruit = firstItem(items: fruits) {
-    print("The first fruit is \(fruit)")
-} else {
-    print("There are no fruits!")
-}
-
-
-func produceFullName(withFirstName firstName: String, middleName: String?, lastName: String) -> String {
+func createWholeName(firstName: String, middleName: String?, lastName: String) -> String {
     var name = firstName
     if let middleName = middleName {
         name = "\(name) \(middleName)"
@@ -46,5 +36,6 @@ func produceFullName(withFirstName firstName: String, middleName: String?, lastN
     return name
 }
 
-produceFullName(withFirstName: "Jon", middleName: "Bon", lastName: "Jovi")
-produceFullName(withFirstName: "Mary", middleName: nil, lastName: "Doe")
+print(createWholeName(firstName: "Steven", middleName: "Michael", lastName: "McGrath"))
+
+print(createWholeName(firstName: "Steven", middleName: nil, lastName: "McGrath"))
